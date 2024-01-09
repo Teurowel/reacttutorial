464ace1ab78994fb2e8e76b4e22e1ee489fdce5b
장두성 <jds2613@challengersgames.com>
2024-01-09 23:51:46 +0900

    6.1 useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Button from "./Button"
 import styles from "./App.module.css"
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1)
+  
+  console.log("i run all time")
+  
+  useEffect(() => {
+    console.log("CALL API")
+  }, []);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcom back!!</h1>
-      <Button text="What"/>
+      <h1 className={styles.title}>{counter}</h1>
+      {/* <Button text="What"/> */}
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
