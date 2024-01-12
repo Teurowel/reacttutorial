7f07a506db6b636531f41a64e5e5d7fcf14d9812
장두성 <jds2613@challengersgames.com>
2024-01-12 23:21:23 +0900

    7.1 To Do List Part Two

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Button from "./Button"
-import styles from "./App.module.css"
+//import Button from "./Button"
+//import styles from "./App.module.css"
 import { useEffect, useState, memo } from "react";
-import Form1 from "./Form1"
-import Form2 from "./Form2"
+//import Form1 from "./Form1"
+//import Form2 from "./Form2"
 
-const MemorizedForm = memo(Form2)
+//const MemorizedForm = memo(Form2)
 
 function App() {
   // const [counter, setValue] = useState(0);
@@ -36,23 +36,55 @@ function App() {
   //   </div>
   // );
 
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  // const [showing, setShowing] = useState(false);
+  // const onClick = () => setShowing((prev) => !prev);
 
-  const [isChecked, setIsChecked] = useState(false);
-  const onIsCheckedClicked = () => setIsChecked((prev) => !prev)
+  // const [isChecked, setIsChecked] = useState(false);
+  // const onIsCheckedClicked = () => setIsChecked((prev) => !prev)
 
-  console.log("App rerender", {isChecked})
-  return(
+  // console.log("App rerender", {isChecked})
+  // return(
+  //   <div>
+  //     {/* {showing ? <Form1 isChecked={isChecked}/> : <Form2 />} */}
+  //     <Form1 isChecked={isChecked}/>
+  //     <MemorizedForm />
+  //     <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
+  //     <button onClick={onIsCheckedClicked}>{isChecked? "Checked" : "Not checked"}</button>
+  //   </div>
+  // );
+
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); 
+    
+    if(toDo === ""){
+      return;
+    }
+
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  }
+
+  console.log(toDos)
+
+  return (
     <div>
-      {/* {showing ? <Form1 isChecked={isChecked}/> : <Form2 />} */}
-      <Form1 isChecked={isChecked}/>
-      <MemorizedForm />
-      <button onClick={onClick}>{showing? "Hide" : "Show"}</button>
-      <button onClick={onIsCheckedClicked}>{isChecked? "Checked" : "Not checked"}</button>
+      <h1>My To Dos {(toDos.length)}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
+        <button>Add To Do</button>
+      </form>
+      
+      <hr />
+      
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
     </div>
-  );
-   
+  );   
 }
 
 export default App;
